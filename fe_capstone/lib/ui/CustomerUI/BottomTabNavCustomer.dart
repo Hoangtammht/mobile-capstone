28303abf9fd11f25549d9e7d976ff6d410d03928
Hoangtammht <hoangtammht@gmail.com>
2023-11-23 14:50:17 +0700

    Back up auto cancel and later

--- fe_capstone/lib/ui/CustomerUI/BottomTabNavCustomer.dart
+++ fe_capstone/lib/ui/CustomerUI/BottomTabNavCustomer.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 import 'dart:convert';
 
 import 'package:fe_capstone/apis/customer/HomeAPI.dart';
+import 'package:fe_capstone/apis/customer/ReservationAPI.dart';
 import 'package:fe_capstone/blocs/UserPreferences.dart';
 import 'package:fe_capstone/constant/base_constant.dart';
 import 'package:fe_capstone/models/CustomerHome.dart';
@@ -11,6 +12,7 @@ import 'package:fe_capstone/ui/CustomerUI/HomeScreen.dart';
 import 'package:fe_capstone/ui/CustomerUI/ProfileScreen.dart';
 import 'package:fe_capstone/ui/CustomerUI/WalletScreen.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:persistent_bottom_nav_bar/persistent_tab_view.dart';
 import 'package:web_socket_channel/io.dart';
 import 'package:web_socket_channel/web_socket_channel.dart';
@@ -40,6 +42,111 @@ class _BottomTabNavCustomerState extends State<BottomTabNavCustomer> {
 
   void changeTabHomeScreenController(){
     _controller.jumpToTab(0);
+  }
+
+  String convertToDesiredFormat(String input) {
+    try {
+      DateFormat inputFormat = DateFormat('HH:mm:ss - dd/MM/yyyy');
+      DateTime dateTime = inputFormat.parse(input);
+      DateFormat outputFormat = DateFormat('yyyy-MM-dd HH:mm:ss');
+      return outputFormat.format(dateTime);
+    } catch (e) {
+      print('Error converting date format: $e');
+      return '';
+    }
+  }
+
+  Duration parseDuration(String durationString) {
+    List<String> timeParts = durationString.split(':');
+    int hours = int.parse(timeParts[0]);
+    int minutes = int.parse(timeParts[1]);
+    int seconds = int.parse(timeParts[2]);
+    return Duration(hours: hours, minutes: minutes, seconds: seconds);
+  }
+
+  void callApiBeforeCancelBooking(Duration beforeCancelBooking,
+      DateTime afterCancelBooking, int reservationID, String ploID) async {
+    try {
+      bool isUpdateSuccessful =
+      await Future.delayed(beforeCancelBooking, () async {
+        return await ReservationAPI.updateReservationToCancel(reservationID);
+      });
+      print('Update trước 15 phút là $isUpdateSuccessful');
+      if (isUpdateSuccessful) {
+        print('Hiện tại trước 15 phút');
+      } else {
+        callApiAtCancelBookingTime(
+            afterCancelBooking.difference(DateTime.now()), reservationID, ploID);
+      }
+    } catch (e) {
+      print('Error calling API: $e');
+    }
+  }
+
+  Future<void> callApiAtCancelBookingTime(
+      Duration duration, int reservationID, String ploID) async {
+    try {
+      await Future.delayed(duration, () async {
+        bool isUpdateSuccessful =
+        await ReservationAPI.updateReservationToCancel(reservationID);
+        print('Update sau 15 phút là $isUpdateSuccessful');
+        setState(() {
+          customerHome = _getHomeStatus();
+        });
+        WebSocketChannel ploChannel =
+        IOWebSocketChannel.connect(BaseConstants.WEBSOCKET_PRIVATE_PLO_URL);
+        final message = {
+          "ploID": ploID,
+          "content": "GetParking"
+        };
+        final messageJson = jsonEncode(message);
+        ploChannel.sink.add(messageJson);
+      });
+    } catch (e) {
+      print('Error calling API: $e');
+    }
+  }
+
+  void callApiBeforeUpdateStatusLater(
+      Duration beforeUpdate, DateTime afterUpdate, int reservationID, String ploID) async {
+    try {
+      bool isUpdateSuccessful = await Future.delayed(beforeUpdate, () async {
+        return await ReservationAPI.updateReservationToLater(reservationID);
+      });
+      print('Status của reservation trước khi trễ là $isUpdateSuccessful');
+      if (isUpdateSuccessful) {
+        print('Status hiện tại trước 15 phút');
+      } else {
+        callApiAfterStatusLater(
+            afterUpdate.difference(DateTime.now()), reservationID, ploID);
+      }
+    } catch (e) {
+      print('Error calling API: $e');
+    }
+  }
+
+  Future<void> callApiAfterStatusLater(
+      Duration duration, int reservationID, String ploID) async {
+    try {
+      await Future.delayed(duration, () async {
+        bool isUpdateSuccessful =
+        await ReservationAPI.updateReservationToLater(reservationID);
+        print('Status của reservation sau 15 phút là $isUpdateSuccessful');
+        setState(() {
+          customerHome = _getHomeStatus();
+        });
+        WebSocketChannel ploChannel =
+        IOWebSocketChannel.connect(BaseConstants.WEBSOCKET_PRIVATE_PLO_URL);
+        final ploMessage = {
+          "ploID": ploID.toString(),
+          "content": "GetParking"
+        };
+        final messageJsonPLO = jsonEncode(ploMessage);
+        ploChannel.sink.add(messageJsonPLO);
+      });
+    } catch (e) {
+      print('Error calling API: $e');
+    }
   }
 
   List<Widget> _buildScreens() {
@@ -123,26 +230,38 @@ class _BottomTabNavCustomerState extends State<BottomTabNavCustomer> {
                   customerHome = _getHomeStatus();
                   customerHome!.then((data) {
                     if (data.reservationID != 0) {
-                      WebSocketChannel channel = IOWebSocketChannel.connect(
-                          BaseConstants.WEBSOCKET_PRIVATE_RESERVATION_URL);
-                      bool isLoggedIn = UserPreferences.isLoggedIn();
-                      if (isLoggedIn) {
-                        const duration = Duration(seconds: 45);
-                        Timer.periodic(duration, (Timer t) {
-                          final message = {
-                            "reservationID": data.reservationID.toString(),
-                            "content": "KeepAlive"
-                          };
-                          final messageJson = jsonEncode(message);
-                          if (channel.sink != null) {
-                            channel.sink.add(messageJson);
-                            print('KeepAlive message sent successfully.');
-                          } else {
-                            print('Channel sink is closed. KeepAlive message not sent.');
-                            t.cancel();
-                          }
-                        });
-                      }
+                        if (data.statusID == 1) {
+                          String startTime = convertToDesiredFormat(data.startTime);
+                          String cancelBookingTime = data.cancelBookingTime;
+                          DateTime startDateTime = DateTime.parse(startTime);
+                          Duration cancelBookingDuration = parseDuration(cancelBookingTime);
+                          DateTime beforeCancelBookingTime = startDateTime
+                              .add(cancelBookingDuration)
+                              .subtract(Duration(minutes: 1));
+                          DateTime cancelBookingDateTime = startDateTime
+                              .add(cancelBookingDuration)
+                              .add(Duration(seconds: 3));
+                          callApiBeforeCancelBooking(
+                              beforeCancelBookingTime.difference(DateTime.now()),
+                              cancelBookingDateTime,
+                              data.reservationID,
+                              data.ploID
+                          );
+                        }
+                        if (data.statusID == 2) {
+                          String endTime = convertToDesiredFormat(data.endTime);
+                          DateTime endDateTime = DateTime.parse(endTime);
+                          DateTime beforeUpdateLater =
+                          endDateTime.subtract(Duration(minutes: 1));
+                          DateTime afterUpdateStatusLater =
+                          endDateTime.add(Duration(seconds: 3));
+                          callApiBeforeUpdateStatusLater(
+                              beforeUpdateLater.difference(DateTime.now()),
+                              afterUpdateStatusLater,
+                              data.reservationID,
+                              data.ploID
+                          );
+                        }
                     }
                   });
                 });
